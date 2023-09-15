12897aa71c3d9527074d2d951ab42f3434ecd870
Sauloteles <105803651+Saulot-dev@users.noreply.github.com>
2023-09-14 23:58:36 -0300

    Todo editado

--- ToDo.py
+++ ToDo.py
@@ -4,3 +4,8 @@
 Treinar Classes:
     Criar classes com atributos e métodos razoáveis;
     Usar conceitos de Cálculo e G.A.;
+    
+    criar repos para projetos almejados:
+        ediçao de video
+        para todos os aprendizados
+        
